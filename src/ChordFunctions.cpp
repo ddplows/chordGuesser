093d83d08f3b53d6a5125631eb5d4fbba176880a
ddplows <ddplows@gmail.com>
2017-01-18 13:07:44 -0500

    Ran astyle

--- src/ChordFunctions.cpp
+++ src/ChordFunctions.cpp
@@ -11,46 +11,47 @@
 //most guitar chords are played on the lowest frets though
 //unless barred I guess
 //idk
-std::string chooseChord(std::vector<int8_t> chordInfo) {
-		std::string closestChord;
-		static int diff = 10000;
-		for (auto const& ent1 : chordCatalog) {
-			int diff2 = 0;
-			// ent1.first is key
-			// ent1.second is value
-			for (unsigned int i = 0; i < ent1.second.size(); i++) {
-				//do not compare unplayed chord notes
-				if (ent1.second[i] != -1 || chordInfo[i] != 0) {
-					diff2 += abs(ent1.second[i] - chordInfo[i]);
-				}
-			}
-			if (diff2 < diff) {
-				diff = diff2;
-				closestChord = ent1.first;
-			}
-		}
-		return closestChord;
+std::string chooseChord(std::vector<int8_t> chordInfo)
+{
+    std::string closestChord;
+    static int diff = 10000;
+    for (auto const& ent1 : chordCatalog) {
+        int diff2 = 0;
+        // ent1.first is key
+        // ent1.second is value
+        for (unsigned int i = 0; i < ent1.second.size(); i++) {
+            //do not compare unplayed chord notes
+            if (ent1.second[i] != -1 || chordInfo[i] != 0) {
+                diff2 += abs(ent1.second[i] - chordInfo[i]);
+            }
+        }
+        if (diff2 < diff) {
+            diff = diff2;
+            closestChord = ent1.first;
+        }
+    }
+    return closestChord;
 }
 
 //it may be better/faster to just save chord's midi arrays by themselves rather than using this converter function
-std::vector<int8_t> chordToMidi(std::vector<int8_t> fretArr) {
-	std::vector<int8_t> retVec;
-	for (unsigned int i = 0; i < fretArr.size(); i++) {
-		//I'm just putting this in until we decide how to properly handle unplayed strings in chords
-		//skip conversion for string
-		if(fretArr[i] == -1){
-			continue;
-		}
-		if (i == 0) {
-			retVec.push_back(fretArr[i] + 4);
-		}
-		else if (i == 1) {
-			retVec.push_back(fretArr[i] + 11);
-		} else {
-			retVec.push_back(fretArr[i] + (i * 7 + 5));
-		}
-	}
-	return retVec;
+std::vector<int8_t> chordToMidi(std::vector<int8_t> fretArr)
+{
+    std::vector<int8_t> retVec;
+    for (unsigned int i = 0; i < fretArr.size(); i++) {
+        //I'm just putting this in until we decide how to properly handle unplayed strings in chords
+        //skip conversion for string
+        if(fretArr[i] == -1) {
+            continue;
+        }
+        if (i == 0) {
+            retVec.push_back(fretArr[i] + 4);
+        } else if (i == 1) {
+            retVec.push_back(fretArr[i] + 11);
+        } else {
+            retVec.push_back(fretArr[i] + (i * 7 + 5));
+        }
+    }
+    return retVec;
 }
 
 
--- src/ChordFunctions.h
+++ src/ChordFunctions.h
@@ -14,16 +14,16 @@
 #include <cstdint>
 
 static std::map<std::string, std::vector<int8_t>> chordCatalog = {
-		{"E", std::vector<int8_t> { 0, 2, 2, 1, 0, 0 }},
-		{"A", std::vector<int8_t> { -1, 0, 2, 2, 2, 0 }},
-		{"D", std::vector<int8_t> { -1, -1, 0, 2, 3, 2 }},
-		{"G", std::vector<int8_t> { 3, 2, 0, 0, 0, 3 }},
-		{"C", std::vector<int8_t> { -1, 3, 2, 0, 1, 0 }},
-		{"E minor", std::vector<int8_t> { 0, 2, 2, 0, 0, 0 }},
-		{"A minor", std::vector<int8_t> { 0, 2, 2, 1, 0, 0 }},
-		{"D7", std::vector<int8_t> { -1, -1, 0, 2, 1, 2 }},
-		{"A7", std::vector<int8_t> { -1, 0, 2, 0, 2, 0 }},
-		{"F", std::vector<int8_t> { -1, -1, 3, 2, 1, 1 }}
+    {"E", std::vector<int8_t> { 0, 2, 2, 1, 0, 0 }},
+    {"A", std::vector<int8_t> { -1, 0, 2, 2, 2, 0 }},
+    {"D", std::vector<int8_t> { -1, -1, 0, 2, 3, 2 }},
+    {"G", std::vector<int8_t> { 3, 2, 0, 0, 0, 3 }},
+    {"C", std::vector<int8_t> { -1, 3, 2, 0, 1, 0 }},
+    {"E minor", std::vector<int8_t> { 0, 2, 2, 0, 0, 0 }},
+    {"A minor", std::vector<int8_t> { 0, 2, 2, 1, 0, 0 }},
+    {"D7", std::vector<int8_t> { -1, -1, 0, 2, 1, 2 }},
+    {"A7", std::vector<int8_t> { -1, 0, 2, 0, 2, 0 }},
+    {"F", std::vector<int8_t> { -1, -1, 3, 2, 1, 1 }}
 };
 
 std::string chooseChord(std::vector<int8_t> chordInfo);
